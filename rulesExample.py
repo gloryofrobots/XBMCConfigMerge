5c598f3cb1d4f7ce4a233df8eb06405e829c929a
Alex Birulia <gloryofrobots@gmail.com>
2013-01-02 21:08:11 +0200

    rulesExample.py add more info

--- rulesExample.py
+++ rulesExample.py
@@ -1,5 +1,8 @@
 merger.addAction( "SingleShot", GraphQuery(("general","systemtotaluptime")) )
-merger.addAction( "SectionIntelligence", GraphQuery(("myvideos",)) )
+
+merger.addAction( "SectionIntelligence", GraphQuery(("TagName",), Arg = ("AttributeName","AtributeVale") )
+"""<TagName AttributeName = AtributeVale></TagName>"""
+
 merger.addAction( "SectionIntelligence", GraphQuery(("mymusic",)), BlackList = ["scanning2"] )
 merger.addAction( "SectionIntelligence", GraphQuery(("viewstates",)) )
 
